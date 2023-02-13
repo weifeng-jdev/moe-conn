eea4e9c98d3eb829447a557a82df44569bde97a1
卫峰 <im.wf@outlook.com>
2023-02-13 21:30:11 +0800

    feat：增加数据库连接池配置

--- moe-conn-server/src/main/java/com/amano/moeconn/controller/UserAuthenticationController.java
+++ moe-conn-server/src/main/java/com/amano/moeconn/controller/UserAuthenticationController.java
@@ -11,13 +11,8 @@ import com.google.code.kaptcha.Producer;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.imageio.ImageIO;
